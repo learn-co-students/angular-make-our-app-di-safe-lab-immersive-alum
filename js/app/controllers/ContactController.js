eebd1edb821b1495adac4a3f86317761b812142d
Matthew Talbot <matthewtalbot@nypl.org>
2019-08-21 16:12:25 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,10 +1,12 @@
-function ContactController($scope, $timeout) {
-	$scope.name = 'Bill Gates';
+function ContactController(bung, buung) {
+	bung.name = 'Bill Gates';
 
-	$timeout(function () {
-		$scope.name = 'Steve Jobs';
+	buung(function () {
+		bung.name = 'Steve Jobs';
 	}, 5000);
 }
+
+ContactController.$inject = ['$scope', '$timeout']
 
 angular
 	.module('app')
